d2f0bf0e44567f122dbc5a24783224cfe3697237
Sergey Kolupaev <skolupaev@keepersecurity.com>
2019-11-08 17:36:24 -0800

    Readme.md

--- unit_tests/test_sync_down.py
+++ unit_tests/test_sync_down.py
@@ -24,16 +24,15 @@ class TestSyncDown(TestCase):
         auth = get_connected_auth_context()
         vault = Vault(auth)
 
-        self.assertEqual(len(vault.records), 3)
-        self.assertEqual(len(vault.shared_folders), 1)
-        self.assertEqual(len(vault.teams), 1)
-        self.assert_key_unencrypted(vault)
+        self.assertEqual(vault.record_count, 3)
+        self.assertEqual(vault.shared_folder_count, 1)
+        self.assertEqual(vault.team_count, 1)
 
     def test_sync_remove_owned_records(self):
         vault = get_vault()
-        len_before = len(vault.records)
+        len_before = vault.record_count
 
-        records_to_delete = [x.record_uid for x in vault.records.values() if x.owner and not x.shared]
+        records_to_delete = [x.record_uid for x in vault.get_all_records() if x.owner and not x.shared]
 
         def sync_down_removed_records(rq):
             self.assertEqual(rq['command'], 'sync_down')
@@ -46,12 +45,11 @@ class TestSyncDown(TestCase):
         vault.sync_down()
         self.assertTrue(KeeperApiHelper.is_expect_empty())
 
-        self.assertEqual(len(vault.records), len_before - len(records_to_delete))
-        self.assert_key_unencrypted(vault)
+        self.assertEqual(vault.record_count, len_before - len(records_to_delete))
 
     def test_sync_remove_team(self):
         vault = get_vault()
-        teams_to_delete = [x.team_uid for x in vault.teams.values()]
+        teams_to_delete = [x.team_uid for x in vault.get_all_teams()]
 
         def sync_down_removed_teams(rq):
             self.assertEqual(rq['command'], 'sync_down')
@@ -64,13 +62,12 @@ class TestSyncDown(TestCase):
         vault.sync_down()
         self.assertTrue(KeeperApiHelper.is_expect_empty())
 
-        self.assertEqual(len(vault.records), 3)
-        self.assertEqual(len(vault.teams), 0)
-        self.assert_key_unencrypted(vault)
+        self.assertEqual(vault.record_count, 3)
+        self.assertEqual(vault.team_count, 0)
 
     def test_sync_remove_shared_folder_then_team(self):
         vault = get_vault()
-        sf_to_delete = [x.shared_folder_uid for x in vault.shared_folders.values()]
+        sf_to_delete = [x.shared_folder_uid for x in vault.get_all_shared_folders()]
 
         def sync_down_removed_shared_folders(rq):
             self.assertEqual(rq['command'], 'sync_down')
@@ -83,12 +80,11 @@ class TestSyncDown(TestCase):
         vault.sync_down()
         self.assertTrue(KeeperApiHelper.is_expect_empty())
 
-        self.assertEqual(len(vault.records), 3)
-        self.assertEqual(len(vault.shared_folders), 1)
-        self.assertEqual(len(vault.teams), 1)
-        self.assert_key_unencrypted(vault)
+        self.assertEqual(vault.record_count, 3)
+        self.assertEqual(vault.shared_folder_count, 1)
+        self.assertEqual(vault.team_count, 1)
 
-        teams_to_delete = [x.team_uid for x in vault.teams.values()]
+        teams_to_delete = [x.team_uid for x in vault.get_all_teams()]
 
         def sync_down_removed_teams(rq):
             self.assertEqual(rq['command'], 'sync_down')
@@ -101,15 +97,14 @@ class TestSyncDown(TestCase):
         vault.sync_down()
         self.assertTrue(KeeperApiHelper.is_expect_empty())
 
-        self.assertEqual(len(vault.records), 2)
-        self.assertEqual(len(vault.shared_folders), 0)
-        self.assertEqual(len(vault.teams), 0)
-        self.assert_key_unencrypted(vault)
+        self.assertEqual(vault.record_count, 2)
+        self.assertEqual(vault.shared_folder_count, 0)
+        self.assertEqual(vault.team_count, 0)
 
     def test_sync_remove_team_shared_folder(self):
         vault = get_vault()
-        teams_to_delete = [x.team_uid for x in vault.teams.values()]
-        sf_to_delete = [x.shared_folder_uid for x in vault.shared_folders.values()]
+        teams_to_delete = [x.team_uid for x in vault.get_all_teams()]
+        sf_to_delete = [x.shared_folder_uid for x in vault.get_all_shared_folders()]
 
         def sync_down_removed_teams_and_shared_folders(rq):
             self.assertEqual(rq['command'], 'sync_down')
@@ -123,24 +118,6 @@ class TestSyncDown(TestCase):
         vault.sync_down()
         self.assertTrue(KeeperApiHelper.is_expect_empty())
 
-        self.assertEqual(len(vault.records), 2)
-        self.assertEqual(len(vault.shared_folders), 0)
-        self.assertEqual(len(vault.teams), 0)
-        self.assert_key_unencrypted(vault)
-
-    def assert_key_unencrypted(self, vault):
-        # type: (Vault) -> NoReturn
-        for r in vault.records.values():
-            self.assertIsNotNone(r.record_key)
-        for sf in vault.shared_folders.values():
-            self.assertIsNotNone(sf.shared_folder_key)
-        for t in vault.teams.values():
-            self.assertIsNotNone(t.team_key)
-
-    # def test_convert_to_folders(self):
-    #     params = get_synced_params()
-    #
-    #     KeeperApiHelper.communicate_expect(['convert_to_folders'])
-    #     result = convert_to_folders(params)
-    #     self.assertTrue(KeeperApiHelper.is_expect_empty())
-    #     self.assertTrue(result)
+        self.assertEqual(vault.record_count, 2)
+        self.assertEqual(vault.shared_folder_count, 0)
+        self.assertEqual(vault.team_count, 0)
