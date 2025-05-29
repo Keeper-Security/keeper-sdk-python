5269c97e2f33c10cea5ddee071cb66e20d19e9a8
adeshmukh-ks <adeshmukh@keepersecurity.com>
2025-05-29 19:58:08 +0530

    record_type_edit and record_type_delete functions added

--- keepercli-package/src/keepercli/commands/record_type.py
+++ keepercli-package/src/keepercli/commands/record_type.py
@@ -11,68 +11,113 @@ from .. import api
 logger = api.get_logger()
 
 class RecordTypeAddCommand(base.ArgparseCommand):
-    parser = argparse.ArgumentParser(
-        prog='record-type-add',
-        description='Create a custom record type.'
-    )
-    parser.add_argument(
-        '--data',
-        dest='data',
-        action='store',
-        required=True,
-        help='Record type definition in JSON format.'
-    )
 
     def __init__(self):
-        super().__init__(RecordTypeAddCommand.parser)
+        parser = argparse.ArgumentParser(
+            prog='record-type-add',
+            description='Add a new custom record type.'
+        )
+        parser.add_argument(
+            '--data',
+            dest='data',
+            action='store',
+            required=True,
+            help='Record type definition in JSON format or "filepath:" to read from JSON file.'
+        )
+        super().__init__(parser)
 
     def execute(self, context: KeeperParams, **kwargs) -> None:
         if not context.vault:
             raise ValueError("Vault is not initialized.")
 
         data = kwargs.get('data')
-        record_type = None
 
-        if data and data.strip().startswith('filepath:'):
-            filepath = data.split('filepath:')[1].strip()
-            try:
-                with open(filepath, 'r') as file:
-                    data = file.read()
-            except FileNotFoundError:
-                raise ValueError(f"File not found: {filepath}")
-        if not data:
-            raise ValueError("Cannot add record type without definition. Option --data is required.")
+        record_type = load_data(data)
 
-        try:
-            record_type = json.loads(data)
-        except json.JSONDecodeError as e:
-            raise ValueError(f"Invalid JSON format in --data: {e}")
+        is_valid_data(record_type)
 
         title = record_type.get('$id')
         fields = record_type.get('fields')
         description = record_type.get('description', '')
-
-        if not title:
-            raise ValueError("Record type must have a '$id' field.")
-        if not fields or not isinstance(fields, list):
-            raise ValueError("Record type must include a list of 'fields'.")
-
-        # Implicit fields - always present on any record, no need to be specified in the template: title, custom, notes
-        implicit_field_names = [x for x in record_implicit_fields]
-        implicit_fields = [r for r in record_type if r in implicit_field_names]
-        if implicit_fields:
-            error = {'error: Implicit fields not allowed in record type definition: ' + str(implicit_fields)}
-            raise ValueError(error)
-
-        rt_attributes = ('$id', 'categories', 'description', 'fields')
-        bada = [r for r in record_type if r not in rt_attributes and r not in implicit_field_names]
-        if bada:
-            logging.debug(f'Unknown attributes in record type definition: {bada}')
+        categories = record_type.get('categories', [])
 
         result = record_type_management.create_custom_record_type(
-            context.vault, title, fields, description
+            context.vault, title, fields, description, categories
         )
         logger.info(f"Custom record type '{title}' created successfully with fields: {[f['$ref'] for f in fields]} and recordTypeId: {result.recordTypeId}")
+
+
+class RecordTypeEditCommand(base.ArgparseCommand):
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            prog='record-type-edit',
+            description='Update or edit a custom record type.'
+        )
+        parser.add_argument(
+            '--data',
+            dest='data',
+            action='store',
+            required=True,
+            help='Record type definition in JSON format or "filepath:" to read from JSON file.'
+        )
+        parser.add_argument(
+            'record_type_id',
+            type=int,
+            nargs='?',
+            help='Record Type ID of record type to be updated.'
+        )
+        super().__init__(parser)
+
+    def execute(self, context: KeeperParams, **kwargs) -> None:
+        if not context.vault:
+            raise ValueError("Vault is not initialized.")
+
+        data = kwargs.get('data')
+        record_type_id = kwargs.get('record_type_id')
+
+        if not record_type_id:
+            raise ValueError("Missing required argument: record_type_id")
+        
+        record_type = load_data(data)
+
+        is_valid_data(record_type)
+
+        title = record_type.get('$id')
+        fields = record_type.get('fields')
+        description = record_type.get('description', '')
+        categories = record_type.get('categories', [])
+
+        result = record_type_management.edit_custom_record_types(
+            context.vault, record_type_id, title, fields, description, categories
+        )
+        logger.info(f"Custom record type (ID: {record_type_id}) updated successfully with fields: {[f['$ref'] for f in fields]} and recordTypeId: {result.recordTypeId}")
+
+
+class RecordTypeDeleteCommand(base.ArgparseCommand):
+    
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            prog='record-type-delete',
+            description='Delete a custom record type.'
+        )
+        parser.add_argument(
+            'record_type_id',
+            type=int,
+            help='Record Type ID of record type to be deleted.'
+        )
+        super().__init__(parser)
+
+    def execute(self, context: KeeperParams, **kwargs) -> None:
+        if not context.vault:
+            raise ValueError("Vault is not initialized.")
+
+        record_type_id = kwargs.get('record_type_id')
+        if not record_type_id:
+            raise ValueError("Missing required argument: record_type_id.")
+
+        result = record_type_management.delete_custom_record_types(context.vault, record_type_id)
+        logger.info(f"Custom record type deleted successfully with record type id: {result.recordTypeId}")
 
 
 record_implicit_fields = {
@@ -80,3 +125,44 @@ record_implicit_fields = {
     'custom': [],  # Array of Field Data objects
     'notes': ''  # string
 }
+
+
+def is_valid_data(record_type):
+    title = record_type.get('$id')
+    fields = record_type.get('fields')
+
+    if not title:
+        raise ValueError("Record type must have a '$id' field.")
+    if not fields or not isinstance(fields, list):
+        raise ValueError("Record type must include a list of 'fields'.")
+
+    # Implicit fields - always present on any record, no need to be specified in the template: title, custom, notes
+    implicit_field_names = [x for x in record_implicit_fields]
+    implicit_fields = [r for r in record_type if r in implicit_field_names]
+    if implicit_fields:
+        error = {'error: Implicit fields not allowed in record type definition: ' + str(implicit_fields)}
+        raise ValueError(error)
+
+    rt_attributes = ('$id', 'categories', 'description', 'fields')
+    bad_attributes = [r for r in record_type if r not in rt_attributes and r not in implicit_field_names]
+    if bad_attributes:
+        logging.debug(f'Unknown attributes in record type definition: {bad_attributes}')
+
+
+def load_data(data):
+
+    if data and data.strip().startswith('filepath:'):
+        filepath = data.split('filepath:')[1].strip()
+        try:
+            with open(filepath, 'r') as file:
+                data = file.read()
+        except FileNotFoundError:
+            raise ValueError(f"File not found: {filepath}")
+
+    if not data:
+        raise ValueError("Cannot add record type without definition. --data or --file is required.")
+    
+    try:
+        return json.loads(data)
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Invalid JSON format: {e}")
