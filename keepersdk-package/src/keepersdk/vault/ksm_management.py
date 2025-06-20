38b11b87caba8b4e68f9c00564988452df198e2f
adeshmukh-ks <adeshmukh@keepersecurity.com>
2025-06-20 12:49:59 +0530

    secrets-manager-app create and remove commands

--- keepersdk-package/src/keepersdk/vault/ksm_management.py
+++ keepersdk-package/src/keepersdk/vault/ksm_management.py
@@ -1,12 +1,16 @@
 import datetime
+import json
+from typing import Optional
 
-from . import vault_online, ksm
+from . import vault_online, ksm, record_management, vault_types
 from ..proto.APIRequest_pb2 import GetApplicationsSummaryResponse, ApplicationShareType, GetAppInfoRequest, GetAppInfoResponse
 from ..proto.enterprise_pb2 import GENERAL
-from .. import utils
+from ..proto.record_pb2 import ApplicationAddRequest
+from .. import utils, crypto
 
 URL_GET_SUMMARY_API = 'vault/get_applications_summary'
 URL_GET_APP_INFO_API = 'vault/get_app_info'
+URL_CREATE_APP_API = 'vault/application_add'
 CLIENT_SHORT_ID_LENGTH = 8
 
 
@@ -83,6 +87,55 @@ def get_secrets_manager_app(vault: vault_online.VaultOnline, uid_or_name: str) -
         shared_secrets=shared_secrets,
         client_devices=client_list
     )
+
+
+def create_secrets_manager_app(vault: vault_online.VaultOnline, name: str, force_add: Optional[bool] = False):
+    
+    existing_app = next((r for r in vault.vault_data.records() if r.title == name), None)
+    if existing_app and not force_add:
+        raise ValueError(f'Application with the same name {name} already exists.')
+
+    app_record_data = {
+        'title': name,
+        'type': 'app'
+    }
+
+    data_json = json.dumps(app_record_data)
+    record_key_unencrypted = utils.generate_aes_key()
+    record_key_encrypted = crypto.encrypt_aes_v2(record_key_unencrypted, vault.keeper_auth.auth_context.data_key)
+
+    app_record_uid_str = utils.generate_uid()
+    app_record_uid = utils.base64_url_decode(app_record_uid_str)
+
+    rdata = bytes(data_json, 'utf-8')
+    rdata = crypto.encrypt_aes_v2(rdata, record_key_unencrypted)
+
+    client_modified_time = utils.current_milli_time()
+
+    ra = ApplicationAddRequest()
+    ra.app_uid = app_record_uid
+    ra.record_key = record_key_encrypted
+    ra.client_modified_time = client_modified_time
+    ra.data = rdata
+
+    vault.keeper_auth.execute_auth_rest(request=ra, rest_endpoint=URL_CREATE_APP_API, response_type=None)
+    
+    app_uid_str = utils.base64_url_encode(ra.app_uid)
+    return app_uid_str
+
+
+def remove_secrets_manager_app(vault: vault_online.VaultOnline, uid_or_name: str, force: Optional[bool] = False):
+    
+    app = get_secrets_manager_app(vault=vault, uid_or_name=uid_or_name)
+    
+    if (app.records != 0 or app.folders != 0 or app.count != 0) and not force:
+        raise ValueError('Cannot remove application with clients, shared record, shared folder. Force remove to proceed')
+    
+    record_obj = vault_types.RecordPath(folder_uid=None, record_uid=app.uid)
+    
+    record_management.delete_vault_objects(vault=vault, vault_objects=[record_obj])
+    
+    return app.uid
 
 
 def get_app_info(vault: vault_online.VaultOnline, app_uid):
