ed684836d468ee2109f000412e50da5f30981fb3
adeshmukh-ks <adeshmukh@keepersecurity.com>
2026-01-16 19:03:57 +0530

    review changes and device approve bug fix

--- keepersdk-package/src/keepersdk/authentication/two_fa_utils.py
+++ keepersdk-package/src/keepersdk/authentication/two_fa_utils.py
@@ -1,13 +1,23 @@
 from ..proto import APIRequest_pb2
 from ..vault import vault_online
 
+# REST endpoint constants
+REST_ENDPOINT_2FA_LIST = 'authentication/2fa_list'
+REST_ENDPOINT_2FA_ADD = 'authentication/2fa_add'
+REST_ENDPOINT_2FA_ADD_VALIDATE = 'authentication/2fa_add_validate'
+REST_ENDPOINT_2FA_DELETE = 'authentication/2fa_delete'
+
+DEFAULT_DUO_PUSH_TYPE = APIRequest_pb2.TWO_FA_PUSH_NONE
+DEFAULT_EXPIRE_IN = APIRequest_pb2.TWO_FA_EXP_IMMEDIATELY
+
+
 def get_two_fa_list(vault: vault_online.VaultOnline) -> APIRequest_pb2.TwoFactorListResponse:
-    response = vault.keeper_auth.execute_auth_rest(
-        rest_endpoint='authentication/2fa_list',
+    """Retrieve the list of two-factor authentication methods."""
+    return vault.keeper_auth.execute_auth_rest(
+        rest_endpoint=REST_ENDPOINT_2FA_LIST,
         request=None,
         response_type=APIRequest_pb2.TwoFactorListResponse
     )
-    return response
 
 
 def add_two_fa_method(
@@ -16,22 +26,21 @@ def add_two_fa_method(
     channel_uid: bytes,
     channel_name: str = '',
     phone_number: str = '',
-    duo_push_type: APIRequest_pb2.TwoFactorPushType = APIRequest_pb2.TWO_FA_PUSH_NONE
+    duo_push_type: APIRequest_pb2.TwoFactorPushType = DEFAULT_DUO_PUSH_TYPE
 ) -> APIRequest_pb2.TwoFactorAddResponse:
-
-    rq = APIRequest_pb2.TwoFactorAddRequest()
-    rq.channelType = channel_type
-    rq.channel_uid = channel_uid
-    rq.channelName = channel_name
-    rq.phoneNumber = phone_number
-    rq.duoPushType = duo_push_type
+    """Add a new two-factor authentication method."""
+    request = APIRequest_pb2.TwoFactorAddRequest()
+    request.channelType = channel_type
+    request.channel_uid = channel_uid
+    request.channelName = channel_name
+    request.phoneNumber = phone_number
+    request.duoPushType = duo_push_type
     
-    response = vault.keeper_auth.execute_auth_rest(
-        rest_endpoint='authentication/2fa_add',
-        request=rq,
+    return vault.keeper_auth.execute_auth_rest(
+        rest_endpoint=REST_ENDPOINT_2FA_ADD,
+        request=request,
         response_type=APIRequest_pb2.TwoFactorAddResponse
     )
-    return response
 
 
 def validate_two_fa_method(
@@ -39,26 +48,29 @@ def validate_two_fa_method(
     channel_uid: bytes,
     value_type: APIRequest_pb2.TwoFactorValueType,
     value: str,
-    expire_in: APIRequest_pb2.TwoFactorExpiration = APIRequest_pb2.TWO_FA_EXP_IMMEDIATELY
+    expire_in: APIRequest_pb2.TwoFactorExpiration = DEFAULT_EXPIRE_IN
 ) -> None:
-
-    rq = APIRequest_pb2.TwoFactorValidateRequest()
-    rq.channel_uid = channel_uid
-    rq.valueType = value_type
-    rq.value = value
-    rq.expireIn = expire_in
+    """Validate a two-factor authentication method."""
+    request = APIRequest_pb2.TwoFactorValidateRequest()
+    request.channel_uid = channel_uid
+    request.valueType = value_type
+    request.value = value
+    request.expireIn = expire_in
     
     vault.keeper_auth.execute_auth_rest(
-        rest_endpoint='authentication/2fa_add_validate',
-        request=rq,
+        rest_endpoint=REST_ENDPOINT_2FA_ADD_VALIDATE,
+        request=request,
         response_type=None
     )
 
+
 def delete_two_fa_method(vault: vault_online.VaultOnline, channel_uid: bytes) -> None:
-    rq = APIRequest_pb2.TwoFactorDeleteRequest()
-    rq.channel_uid = channel_uid
+    """Delete a two-factor authentication method."""
+    request = APIRequest_pb2.TwoFactorDeleteRequest()
+    request.channel_uid = channel_uid
+    
     vault.keeper_auth.execute_auth_rest(
-        rest_endpoint='authentication/2fa_delete',
-        request=rq,
+        rest_endpoint=REST_ENDPOINT_2FA_DELETE,
+        request=request,
         response_type=None
     )
