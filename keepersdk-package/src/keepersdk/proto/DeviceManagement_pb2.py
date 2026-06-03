a9c5d0733befeb94f0ebb5d4c26bb4460be0ccbb
adeshmukh-ks <adeshmukh@keepersecurity.com>
2026-06-03 14:19:49 +0530

    protobuff corrections

--- keepersdk-package/src/keepersdk/proto/DeviceManagement_pb2.py
+++ keepersdk-package/src/keepersdk/proto/DeviceManagement_pb2.py
@@ -6,9 +6,17 @@
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-
+_runtime_version.ValidateProtobufRuntimeVersion(
+    _runtime_version.Domain.PUBLIC,
+    5,
+    29,
+    3,
+    '',
+    'DeviceManagement.proto'
+)
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -70,4 +78,3 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_DEVICEADMINACTIONRESULT']._serialized_start=2113
   _globals['_DEVICEADMINACTIONRESULT']._serialized_end=2322
 # @@protoc_insertion_point(module_scope)
-
